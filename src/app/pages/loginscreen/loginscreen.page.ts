096cff58f054b6ee2cb97ba7427873afbbaedabd
arunprasanthrn <arunprasanthrn@gmail.com>
2021-08-15 21:06:59 +0530

    changes

--- src/app/pages/loginscreen/loginscreen.page.ts
+++ src/app/pages/loginscreen/loginscreen.page.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import { FormGroup,FormBuilder, FormControl,Validators } from '@angular/forms';
 @Component({
   selector: 'app-loginscreen',
   templateUrl: './loginscreen.page.html',
   styleUrls: ['./loginscreen.page.scss'],
 })
 export class LoginscreenPage implements OnInit {
+  validationUserMessage={
+    email:[
+      {type:"required",message:"please enter your email"},
+      {type:"pattern",message:"The entered email is incorrect.Try again."}
+    ],
+    password:[
+      {type:"required",message:"please enter your password"},
+      {type:"pattern",message:"password must be atleast 5 characters"}    ]
+  }
 
-  constructor(public router:Router) { }
+  validationFormUser:FormGroup;
+  constructor(public router:Router ,public formbuilder:FormBuilder) { }
   RedirectToForgotPassword()
   {
     this.router.navigateByUrl('/forgotpassword');
@@ -20,8 +31,23 @@ export class LoginscreenPage implements OnInit {
   {
     this.router.navigateByUrl('/login');
   }
-
+ 
   ngOnInit() {
-  }
+    this.validationFormUser=this.formbuilder.group({
+        email: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ])),
+        password: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.minLength(5)
+        ]))
+      })
+    
+
+    }
+  
+
 
 }
+
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+ export const firebaseConfig = {
+  apiKey: "AIzaSyB9GdJYr137fDy0j83b1iEq_9_wnCdhaMY",
+  authDomain: "loginform-ionics.firebaseapp.com",
+  projectId: "loginform-ionics",
+  storageBucket: "loginform-ionics.appspot.com",
+  messagingSenderId: "392938251589",
+  appId: "1:392938251589:web:02892f7cfd70a362028a2c",
+  measurementId: "G-BM06KGXYV1"
+};
